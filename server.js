e399b32c7e585b8cf8795c9303bb737b19f40b6b
paintedlbird7 <rosaperezlarios@gmail.com>
2019-05-08 15:36:17 -0700

    added Sanity Check

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const server = express();
+
+
+
+// sanity check
+server.get('/', (req, res) => {
+  res.send(`
+    it's working!
+  `);
+});
+
+
+
+
+
+ 
+
+
+module.exports = server;
