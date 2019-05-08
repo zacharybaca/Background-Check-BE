21efc560383e602010fe2b81baddcae8cbecf1df
cubsfan46350 <cubsfan.46350@msn.com>
2019-05-08 16:48:45 -0500

    added dep. created sanity check

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const server = express();
+
+
+
+// Sanity Check
+server.get('/', (req, res) => {
+    res.send(`It's Working!`);
+});
+
+
+
+
+server.listen(3000, () => {
+    console.log('Server is running on Port 3K');
+});
