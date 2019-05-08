021b2706fee6cc4eaef4af5bbdc55bfe00f724ea
Marc Thiongo <marcthuo@gmail.com>
2019-05-08 19:28:41 -0400

    knexfile added seed and migration data added

---
+++ data/migrations/20190508184234__users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+
+      tbl.string('name', 128).notNullable();
+
+      tbl.string('username', 255).notNullable().unique();
+
+      tbl.string('password', 255).notNullable();
+
+      tbl.string('usertype').notNullable();
+
+      tbl.string('email').notNullable().unique();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
