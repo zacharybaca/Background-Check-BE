b987d9820673b152aa59a951fa8fff1980dc023b
cubsfan46350 <cubsfan.46350@msn.com>
2019-05-08 19:22:13 -0500

    added endpoint for get and post

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
+const db = require('./data/db.js');
 const server = express();
 
+//Middleware
+server.use(express.json());
 
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ message: 'Cannot Get Users' });
+        });
+});
 
-// Sanity Check
-server.get('/', (req, res) => {
-    res.send(`It's Working!`);
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    db.insert(user)
+        .then(res => res.status(201).json(res))
+        .catch(err => res.status(500).json(err));
 });
 
 
