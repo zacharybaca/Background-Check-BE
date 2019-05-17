83706819255f5e3260dad0f3e230999aec0150e4
cubsfan46350 <cubsfan.46350@msn.com>
2019-05-17 12:45:07 -0500

    finished passport

--- api/server.js
+++ api/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
 const passportSetup = require('./services/passport-setup');
 const userRouter = require('../users/userRouter.js');
 const candidateRouter = require('../candidates/candidateRouter');
+const cookieSession = require('cookie-session');
+const keys = require('./services/keys');
+const passport = require('passport');
 
 
 const server = express();
@@ -13,8 +17,20 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use('/auth',authRoutes);
+server.use('/profile', profileRoutes);
 server.use('/users', userRouter);
 server.use('/candidates', candidateRouter);
+
+server.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+//Initialize Passport
+server.use(passport.initialize());
+server.use(passport.session());
+
+
 
 // sanity check
 server.get('/', (req, res) => {
--- api/services/passport-setup.js
+++ api/services/passport-setup.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../../users/userModel').add;
+const DBUser = require('../../users/userModel');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    DBUser.findById(id).then((user) => {
+        done(null, user);
+    });
+    
+});
 
 passport.use(new GoogleStrategy({
     //options for google strategy
@@ -8,6 +21,20 @@ passport.use(new GoogleStrategy({
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
 }, (accessToken, refreshToken, profile, done) => {
-    //passport callback function
-})
-)
+    //Check If User Already Exists in Our DB
+    DBUser.find({googleid: profile.id}).then((currentUser) => {
+        if(currentUser){
+            console.log('user is: ', currentUser);
+            done(null, currentUser);
+        } else {
+            new User({
+                username: profile.displayName,
+                googleid: profile.id
+            }).save().then((newUser) => {
+                console.log('new user created: ' + newUser);
+                done(null, newUser);
+            });
+        }
+    });
+    
+}));
