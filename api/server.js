8d30c3a219a00933282ff02f6c6de4e7fb5cc04c
Rosa Perez <rosaperez@Rosas-MBP.attlocal.net>
2019-05-17 07:31:47 -0700

    Changed BE to react-stripe-elements

--- api/server.js
+++ api/server.js
@@ -8,6 +8,10 @@ const candidateRouter = require('../candidates/candidateRouter');
 
 const server = express();
 
+const app = require("express")();
+const stripe = require("stripe")("pk_test_hJ4ymeWUIsyUjYOAiTXmMMUG00HWO2eMEX");
+
+app.use(require("body-parser").text());
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -19,5 +23,23 @@ server.use('/candidates', candidateRouter);
 server.get('/', (req, res) => {
     res.status(200).json('API is running');
 });
+
+app.post("/charge", async (req, res) => {
+    try {
+      let {status} = await stripe.charges.create({
+        amount: 2000,
+        currency: "usd",
+        description: "An example charge",
+        source: req.body
+      });
+  
+      res.json({status});
+    } catch (err) {
+      res.status(500).end();
+    }
+  });
+
+  app.listen(9000, () => console.log("Listening on port 9000"));
+
 
 module.exports = server;
