23c425ba514bbe53f77f03e30392613ac6692fb9
Marc Thiongo <marcthuo@gmail.com>
2019-05-20 21:48:50 -0400

    updates to index and server

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const server = express();
 const keys = require('../api/services/keys');
 
 const app = require("express")();
-const stripe = require("stripe")("pk_test_hJ4ymeWUIsyUjYOAiTXmMMUG00HWO2eMEX");
+const stripe = require("stripe")("sk_test_ZhPRczzPc38M67taa8x14w2N00IesVU1LX");
 
 app.use(require("body-parser").text());
 
@@ -61,8 +61,5 @@ server.post("/charge", async (req, res) => {
       res.status(500).end();
     }
   });
-
-  server.listen(9000, () => console.log("Listening on port 9000"));
-
 
 module.exports = server;
--- index.js
+++ index.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
 console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
-//comment
